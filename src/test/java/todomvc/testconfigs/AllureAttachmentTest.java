92bd8ce3da5f719f49283a16ad8799390fcb9f01
MiklePvo <mishmail@ukr.net>
2016-10-30 19:59:47 +0200

    add tests

---
+++ src/test/java/todomvc/testconfigs/AllureAttachmentTest.java
@@ -0,0 +1,23 @@
+package todomvc.testconfigs;
+
+
+import com.codeborne.selenide.Screenshots;
+import com.google.common.io.Files;
+import org.junit.After;
+import ru.yandex.qatools.allure.annotations.Attachment;
+
+import java.io.File;
+import java.io.IOException;
+
+public class AllureAttachmentTest {
+    @After
+    public void tearDown() throws IOException {
+        screenshot();
+    }
+
+    @Attachment(type = "image/png")
+    public static byte[] screenshot() throws IOException {
+        File screenshot = Screenshots.takeScreenShotAsFile();
+        return Files.toByteArray(screenshot);
+    }
+}
