ca7b37c213d6175d23ffd87c1a16993b14c4662e
Mikle100500 <mikhail.pavlenko84@gmail.com>
2016-11-01 11:34:22 +0200

    edit pom

--- src/test/java/com/todomvc/testsuites/AllSuiteTest.java
+++ src/test/java/com/todomvc/testsuites/AllSuiteTest.java
@@ -1,9 +1,9 @@
 package com.todomvc.testsuites;
 
+import com.todomvc.TodosOperationsAtAllFilterTest;
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import com.todomvc.TodosOperationsAtAllFilterTest;
 
 @RunWith(Categories.class)
 @Suite.SuiteClasses(TodosOperationsAtAllFilterTest.class)
