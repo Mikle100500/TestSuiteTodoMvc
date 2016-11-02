5b84fad489b4bbd7a30930c811135f0ece4379e5
mpavlenko <mikle.pavlenko84@gmail.com>
2016-11-02 11:21:35 +0200

    ready for review

--- src/test/java/com/todomvc/testsuites/BuggySuiteTest.java
+++ src/test/java/com/todomvc/testsuites/BuggySuiteTest.java
@@ -1,5 +1,6 @@
 package com.todomvc.testsuites;
 
+import com.todomvc.TodosE2ETest;
 import com.todomvc.TodosOperationsAtAllFilterTest;
 import com.todomvc.categories.Buggy;
 import org.junit.experimental.categories.Categories;
@@ -7,7 +8,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Categories.class)
-@Suite.SuiteClasses(TodosOperationsAtAllFilterTest.class)
+@Suite.SuiteClasses({TodosOperationsAtAllFilterTest.class, TodosE2ETest.class})
 @Categories.IncludeCategory(Buggy.class)
 public class BuggySuiteTest {
 }
--- src/test/java/com/todomvc/testsuites/SmokeSuiteTest.java
+++ src/test/java/com/todomvc/testsuites/SmokeSuiteTest.java
@@ -1,13 +1,14 @@
 package com.todomvc.testsuites;
 
 import com.todomvc.TodosE2ETest;
+import com.todomvc.TodosOperationsAtAllFilterTest;
 import com.todomvc.categories.Smoke;
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Categories.class)
-@Suite.SuiteClasses(TodosE2ETest.class)
+@Suite.SuiteClasses({TodosOperationsAtAllFilterTest.class, TodosE2ETest.class})
 @Categories.IncludeCategory(Smoke.class)
 public class SmokeSuiteTest {
 }
